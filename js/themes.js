8f68eeabeef13434913210cef40ac8f06746221a
Willian Araujo <120990795+will4raujo@users.noreply.github.com>
2023-05-14 19:10:56 -0300

    Melhorando o código, acrescentando mais funcionalidades

--- js/themes.js
+++ js/themes.js
@@ -1,73 +1,155 @@
-const themeButtons = {
-    florest: document.querySelector('#florest'),
-    rain: document.querySelector('#rain'),
-    coffeeShop: document.querySelector('#coffee-shop'),
-    fireplace: document.querySelector('#fireplace'),
+function createThemeElements(theme) {
+  const button = document.querySelector(`#${theme}`);
+  const icon = document.querySelector(`#${theme} svg path`);
+  const sound = new Audio(`./wavs/${theme}.wav`);
+  const volume = document.querySelector(`#${theme} .volume-bar input[type="range"]`);
+
+  sound.loop = true;
+  volume.value = 50;
+
+  return {
+    button,
+    icon,
+    sound,
+    volume
+  };
 }
 
-const themeIcons = {
-    florest: document.querySelector('#florest svg path'),
-    rain: document.querySelector('#rain svg path'),
-    coffeeShop: document.querySelector('#coffee-shop svg path'),
-    fireplace: document.querySelector('#fireplace svg path'),
-}
 
-const themeSounds = {
-    florest: new Audio('./wavs/florest.wav'),
-    rain: new Audio('./wavs/rain.wav'),
-    coffeeShop: new Audio('./wavs/coffee-shop.wav'),
-    fireplace: new Audio('./wavs/fireplace.wav'),
-}
+const florest = createThemeElements('florest');
+const rain = createThemeElements('rain');
+const coffeeShop = createThemeElements('coffee-shop');
+const fireplace = createThemeElements('fireplace');
 
-for ( const soundInLoop in themeSounds){ 
-    themeSounds[soundInLoop].loop = true;
-}
+const themes = {
+  florest,
+  rain,
+  coffeeShop,
+  fireplace,
+};
 
-function resetSound(sound){
-    sound.pause();
-    sound.currentTime = 0;
-}
+let clickedOnVolume = false;
 
-function handleButtonClick(button, icon, sound){
-    if(button.classList.contains('selected')) {
-        button.classList.remove('selected');
-        icon.classList.remove('selected');
-        resetSound(sound);
-    }else {
+function handleButtonClick(theme) {
 
-    for (const buttonKey in themeButtons) {
-        themeButtons[buttonKey].classList.remove('selected')
-      }
+  const { button, icon, sound, volume } = theme;
 
-    for (const iconKey in themeIcons) {
-        themeIcons[iconKey].classList.remove('selected')
-    }
+  volume.addEventListener('click', () => {
+    clickedOnVolume = true;
+  })
 
-    for(const soundKey in themeSounds) {
-        themeSounds[soundKey].pause();
-        resetSound(themeSounds[soundKey]);
+
+  if (button.classList.contains('selected') && clickedOnVolume == false) {
+    button.classList.remove('selected');
+    icon.classList.remove('selected');
+    volume.classList.remove('selected');
+    resetSound(sound);
+
+  } else {
+    for (const themeKey in themes) {
+      const { button, icon, sound, volume } = themes[themeKey];
+      button.classList.remove('selected');
+      icon.classList.remove('selected');
+      volume.classList.remove('selected');
+      resetSound(sound);
     }
 
     button.classList.add('selected');
     icon.classList.add('selected');
+    volume.classList.add('selected');
+    clickedOnVolume = false;
     sound.play();
+  }
 
-    }
+
 }
 
-themeButtons.florest.addEventListener('click', () => {
-    handleButtonClick(themeButtons.florest, themeIcons.florest, themeSounds.florest)
-});
-themeButtons.rain.addEventListener('click', () => {
-    handleButtonClick(themeButtons.rain, themeIcons.rain, themeSounds.rain)
-});
-themeButtons.coffeeShop.addEventListener('click', () => {
-    handleButtonClick(themeButtons.coffeeShop, themeIcons.coffeeShop, themeSounds.coffeeShop)
-});
-themeButtons.fireplace.addEventListener('click', () => {
-    handleButtonClick(themeButtons.fireplace, themeIcons.fireplace, themeSounds.fireplace)
+function resetSound(theme) {
+  theme.pause();
+  theme.currentTime = 0;
+}
+
+florest.button.addEventListener('click', () => {
+  handleButtonClick(florest);
 });
 
+rain.button.addEventListener('click', () => {
+  handleButtonClick(rain);
+});
 
-export {themeButtons, themeIcons, themeSounds}
+coffeeShop.button.addEventListener('click', () => {
+  handleButtonClick(coffeeShop);
+});
 
+fireplace.button.addEventListener('click', () => {
+  handleButtonClick(fireplace);
+});
+
+function controlVolume(theme) {
+  const { sound, volume } = theme
+
+  volume.addEventListener('mousedown', () => {
+    updateVolume()
+  })
+
+  volume.addEventListener('input', () => {
+    updateVolume()
+  })
+
+  function updateVolume() {
+    sound.volume = volume.value / 100;
+  }
+}
+
+florest.volume.addEventListener('click', () => {
+  controlVolume(florest)
+})
+rain.volume.addEventListener('click', () => {
+  controlVolume(rain)
+})
+coffeeShop.volume.addEventListener('click', () => {
+  controlVolume(coffeeShop)
+})
+fireplace.volume.addEventListener('click', () => {
+  controlVolume(fireplace)
+})
+
+const buttonSun = document.querySelector('.sun');
+const buttonMoon = document.querySelector('.moon');
+const controlButtons = document.querySelectorAll('.control-buttons svg path');
+const controlButtonsArray = [...controlButtons];
+
+buttonSun.addEventListener('click', () => {
+  darkMode(florest)
+  darkMode(rain)
+  darkMode(coffeeShop)
+  darkMode(fireplace)
+  hideButtonDarkMode()
+});
+
+buttonMoon.addEventListener('click', () => {
+  darkMode(florest)
+  darkMode(rain)
+  darkMode(coffeeShop)
+  darkMode(fireplace)
+  hideButtonDarkMode()
+})
+
+function hideButtonDarkMode() {
+  buttonMoon.classList.toggle('hide');
+  buttonSun.classList.toggle('hide');
+  document.body.classList.toggle('dark-mode');
+  for (const buttons in controlButtonsArray) {
+    controlButtonsArray[buttons].classList.toggle('dark-mode')
+  }
+}
+
+function darkMode(theme) {
+
+  const { button, icon, sound, volume } = theme;
+
+  button.classList.toggle('dark-mode')
+  icon.classList.toggle('dark-mode')
+  sound.classList.toggle('dark-mode')
+  volume.classList.toggle('dark-mode')
+}
